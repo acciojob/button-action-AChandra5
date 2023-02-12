d1012eaef77e4f0c5b4563b01362e3e2374c121b
Akhil chandra <akhilchandra2309@gmail.com>
2023-02-12 11:33:59 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [clicked, setClicked] = useState(false)
+
+ 
+
   return (
     <div id="main">
       // Do not alter the main div
+      {/* <p id="para">Para</p> */}
+      <button id="click" onClick={()=>{setClicked(!clicked)}}>button</button>
+      {clicked ? <p id="para">para</p> : null}
     </div>
   );
 }
