b4787d4cf2bdda5f4406287a225371c1e97a36aa
Akhil chandra <akhilchandra2309@gmail.com>
2023-02-12 11:53:19 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,7 @@ function App() {
 
   return (
     <div id="main">
-      // Do not alter the main div
-      <button id="click" onClick={renderPara} >button</button>
+      <button id="click" onClick={renderPara}>Click me</button>
       {clicked ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : null}
     </div>
   );
