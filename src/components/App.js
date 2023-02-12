14253806c2dd004a88aff7983359fd8a11e03a71
Akhil chandra <akhilchandra2309@gmail.com>
2023-02-12 11:49:45 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,16 @@ function App() {
 
   const [clicked, setClicked] = useState(false)
 
+  const renderPara= ()=> {
+    setClicked(true)
+  }
+
  
 
   return (
     <div id="main">
       // Do not alter the main div
-      <button id="click" onClick={()=>{setClicked(!clicked)}}>button</button>
+      <button id="click" onClick={renderPara} >button</button>
       {clicked ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : null}
     </div>
   );
